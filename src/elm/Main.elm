6fa778eb5fcc37146fc027a1e9bf3c7b556e141e
Chana Reynolds <chana777@gmail.com>
2017-03-18 11:28:57 -0600

    textInput

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,18 +1,25 @@
 module Main exposing (..)
 
 import Html exposing (program)
-import Msgs exposing (Msg)
-import Models exposing (Model)
-import Update exposing (update)
-import View exposing (view)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
 
 
--- import Speak exposing (..)
+--MODEL
 
 
-init : ( Model, Cmd Msg )
+type alias Model =
+    { dream : String
+    , dreamInput : String
+    }
+
+
+init : Model
 init =
-    ( "Hello", Cmd.none )
+    { dream = ""
+    , dreamInput = ""
+    }
 
 
 subscriptions : Model -> Sub Msg
@@ -21,14 +28,98 @@ subscriptions model =
 
 
 
--- MAIN
+--MAIN
 
 
 main : Program Never Model Msg
 main =
     Html.program
-        { init = init
+        { init = ( init, Cmd.none )
         , view = view
         , update = update
         , subscriptions = subscriptions
         }
+
+
+
+--UPDATE
+
+
+type Msg
+    = SetDreamInput String
+    | SaveDream
+    | CancelDream
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        SetDreamInput value ->
+            ( { model | dreamInput = value }, Cmd.none )
+
+        SaveDream ->
+            if String.isEmpty model.dreamInput then
+                ( model, Cmd.none )
+            else
+                ( { model | dreamInput = model.dreamInput, dreamInput = "" }, Cmd.none )
+
+        CancelDream ->
+            ( { model | dreamInput = "" }, Cmd.none )
+
+
+viewDreamInput : Model -> Html Msg
+viewDreamInput model =
+    div []
+        [ input
+            [ type_ "text"
+            , placeholder "Dream"
+            , value model.dreamInput
+            , autofocus True
+            , onInput SetDreamInput
+            ]
+            []
+        , primaryButton SaveDream "Save"
+        , primaryButton CancelDream "Cancel"
+        ]
+
+
+primaryButton : Msg -> String -> Html Msg
+primaryButton msg name =
+    button [ onClick msg ] [ text name ]
+
+
+viewNav : String -> Html Msg
+viewNav title =
+    header []
+        [ h1 [ class "text-center" ] [ text title ]
+        , ul []
+            [ ul [ attribute "role" "presentation" ]
+                [ a [ href "#" ]
+                    [ text "Profile" ]
+                ]
+            , ul [ attribute "role" "presentation" ]
+                [ a [ href "./static/d3.html" ]
+                    [ text "WordCloud" ]
+                ]
+            ]
+        ]
+
+
+viewFooter : Html Msg
+viewFooter =
+    footer [ class "text-center" ]
+        [ a [ href "https://github.com/Chanaeva" ]
+            [ text "Created By Chana Reynolds" ]
+        ]
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ viewNav "DreamCatcher"
+        , viewDreamInput model
+        , div [ class "button-group" ]
+            []
+        , viewFooter
+        , div [ class "debug" ] [ text (toString model) ]
+        ]
